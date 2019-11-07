eba63d3a4b54862b05dfb70b3ba00324864bb142
maruseu <marceucamp@gmail.com>
2019-11-07 16:27:04 -0300

    initial commit

---
+++ .scripts/display1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 1366x768 --pos 0x0 --rotate normal
---
+++ .scripts/display2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1366x768 --pos 0x768 --rotate normal --output HDMI-0 --primary --mode 1366x768 --pos 0x0 --rotate normal
